1e657e5c7a8874ab048a3d74dbf1bb225138473b
CheaSenghuy <cheasenghuy21@gmail.com>
2024-03-06 10:30:37 +0700

    testing in local

--- app/dashboard/about/page.tsx
+++ app/dashboard/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => {
     return (
-        <div>Hello World I like every single humans on Earth INLOVE with natrue japan and korean </div>
+        <div>Hello World I like every single humanasdf asf sadsasdfas on Earth INLOVE with natrue japan and korean </div>
     )
 }
 
